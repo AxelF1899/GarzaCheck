0f137b790f386ddadd7b9e13854c7f00295979dc
Pablo Axel Flores Bautista <pabloaxelcsr@gmail.com>
2023-10-21 10:45:50 -0600

    Cambios de diseño y usabilidad

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, redirect, url_for, session, render_template, request
 from flask_oauthlib.client import OAuth
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.secret_key = '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'
+app.secret_key = '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' #Guardar y borrar por ahora
+
+scopes = ['https://www.google.com/auth/userinfo.profile', 'https://www.google.com/auth/userinfo.email']
+
 oauth = OAuth(app)
 
 google = oauth.remote_app(
@@ -11,7 +15,7 @@ google = oauth.remote_app(
     consumer_key='875442195195-g3rje2pdqa7iuk0our7h8dfdgcu9qndo.apps.googleusercontent.com',
     consumer_secret='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',
     request_token_params={
-        'scope': 'email profile',  # Puedes personalizar los alcances necesarios
+        'scope': 'email profile',
     },
     base_url='https://www.googleapis.com/oauth2/v1/',
     request_token_url=None,
@@ -20,17 +24,10 @@ google = oauth.remote_app(
     authorize_url='https://accounts.google.com/o/oauth2/auth',
 )
 
-
-'''
-'''
-# Función tokengetter
-
 @app.route('/')
 def landing_page():
-    # Tu contenido de landing page aquí
-    current_year = 2023  # Puedes obtener el año actual de manera dinámica si lo deseas
-
-    return render_template('landing.html', current_year=current_year, user_name='Axel')
+    current_year = 2023
+    return render_template('landing.html', current_year=current_year)
 
 @app.route('/login')
 def login():
@@ -51,52 +48,68 @@ def authorized():
             request.args['error_description']
         )
     
-    # Almacena el token de acceso en la sesión
+    # Almacenamiento del token de acceso en la sesión
     session['google_token'] = (response['access_token'], '')
+    #Verificación de la sesión
+    if 'google_token' in session:
+        access_token = session['google_token'][0]
 
-    # Redirige al usuario a la página de inicio (home)
+        user_info_response = google.get('userinfo', token=(access_token, ''))
+
+        if user_info_response.status == 200:
+            user_data = user_info_response.data
+            user_name = user_data.get('name', 'Nombre no disponible')
+            user_email = user_data.get('email', 'Correo electrónico no disponible')
+        else:
+            print("No se pudo obtener la información del usuario")
+
+        names = user_name.split()
+        first_name = " ".join(names[:-2])
+        father_lastname = names[-2]
+        mother_lastname = names[-1]
+
+        account_number = int(user_email[2:8])
+
+        # Almacena los valores en la sesión para usarlos en otras rutas
+        session['first_name'] = first_name
+        session['father_lastname'] = father_lastname
+        session['mother_lastname'] = mother_lastname
+        session['account_number'] = account_number
+
     return redirect(url_for('home'))
-
 
 @app.route('/home')
 def home():
-    # Esta es la nueva interfaz después de iniciar sesión
-    return render_template('home.html')
+    first_name = session.get('first_name', 'Nombre no disponible')
+    father_lastname = session.get('father_lastname', 'Apellido paterno no disponible')
+    mother_lastname = session.get('mother_lastname', 'Apellido materno no disponible')
+    account_number = session.get('account_number', 'Número de cuenta no disponible')
+
+    return render_template('home.html', first_name=first_name, father_lastname=father_lastname, mother_lastname=mother_lastname, account_number=account_number)
 
 @app.route('/informacion_personal')
 def informacion_personal():
-    # Obtén los datos del usuario, como el grupo, número de cuenta, semestre y carrera, desde donde los almacenes
-    # Puedes guardar estos datos en variables o recuperarlos de una base de datos, según cómo estén almacenados en tu aplicación
-    
-    # Luego, pasa estos datos a tu plantilla HTML
-    return render_template('personal_info.html', grupo=1, numero_cuenta=385309, semestre=5, carrera='Ingeniería en software')
-
+    first_name = session.get('first_name', 'Nombre no disponible')
+    father_lastname = session.get('father_lastname', 'Apellido paterno no disponible')
+    mother_lastname = session.get('mother_lastname', 'Apellido materno no disponible')
+    account_number = session.get('account_number', 'Número de cuenta no disponible')
+    return render_template('personal_info.html', first_name=first_name, father_lastname=father_lastname, mother_lastname=mother_lastname, account_number=account_number, semestre = 1, grupo =1)
 
 @app.route('/editar_informacion_personal', methods=['POST'])
 def editar_informacion_personal():
     if request.method == 'POST':
-        # Obtén los datos enviados por el formulario
+        # Obtención de los datos enviados por el formulario
         grupo = request.form['grupo']
         numero_cuenta = request.form['numero_cuenta']
         semestre = request.form['semestre']
         carrera = request.form['carrera']
-        
-        # Actualiza los datos del usuario con los nuevos valores (esto puede variar según cómo almacenes los datos)
-        # Puedes guardar estos datos en una base de datos, en una variable de sesión, etc.
-        
-        # Después de actualizar los datos, redirige al usuario a la página de información personal
         return redirect(url_for('informacion_personal'))
 
 @app.route('/horarios')
 def horarios():
-    # Obtén los datos del usuario, como el grupo, número de cuenta, semestre y carrera, desde donde los almacenes
-    # Puedes guardar estos datos en variables o recuperarlos de una base de datos, según cómo estén almacenados en tu aplicación
-    
-    # Luego, pasa estos datos a tu plantilla HTML
     return render_template('schedules.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:4202@localhost/project5'
+    db = SQLAlchemy(app)
